9fb220882a968997d72ee97c78cf31a82e629e6d
PriyasMsc <priyangatce@gmail.com>
2020-04-07 19:56:22 +0530

    upto this topic

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import './style.css';
 
 // Write Javascript code!
+// closure
 function showName(firstName, lastName) {
   var nameIntro = "My name is ";
   // this inner function has access to the outer function's variables, including the parameter
@@ -16,10 +17,24 @@ document.getElementById("closure").innerHTML =
   showName("Priyanga", "Subramaniam"); 
   
 
-  
+ // hoist  
 hoist = 5; // Assign 5 to hoist
 
 var elem = document.getElementById("hoist"); // Find an element 
 elem.innerHTML = hoist;           // Display hoist in the element
 
 var hoist; // Declare hoist
+
+// this 
+// Create an object:
+var person = {
+  firstName: "John",
+  lastName : "Doe",
+  id     : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+// Display data from the object:
+document.getElementById("this").innerHTML = person.fullName();
